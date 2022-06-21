6bc44e9c076401fccc179260236688e6e8e9616d
Pete <petersalanoa@gmail.com>
2022-06-21 23:03:10 +0200

    update

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,6 +1,6 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
+    title: 'A Search Engine 6',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
